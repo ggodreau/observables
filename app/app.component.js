9cf1c59566358cd1a2c74f7b94d426e62ff58550
ggodreau <godreau@gmail.com>
2017-03-25 20:37:18 +0000

    not-so-working 2 fail then pass obsv

--- app/app.component.js
+++ app/app.component.js
@@ -45,14 +45,43 @@ System.register(['angular2/core', 'angular2/common', 'rxjs/Rx'], function(export
                         return [1, 2, 3];
                     })
                         .subscribe(function (x) { return console.log(x); });
-                    var observable = Rx_1.Observable.interval(15000);
-                    observable.flatMap(function (x) {
-                        console.log("calling server:");
+                    //        var observable = Observable.interval(15000);
+                    //        observable.flatMap(x => {
+                    //            console.log("calling server:");
+                    //            return Observable.of([1, 2, 3]);
+                    //        })
+                    //        .subscribe(x => console.log(x));
+                    //        var userStream = Observable.of({
+                    //            userId:     1,
+                    //            username:   'mosh'
+                    //        }).delay(2000);
+                    //
+                    //        var tweetsStream = Observable.of([1, 2, 3]).delay(1500);
+                    //
+                    //        Observable
+                    //            .forkJoin(userStream, tweetsStream)
+                    //            .map(joined => 
+                    //                 new Object({user: joined[0], tweets: joined[1]})
+                    //                )
+                    //            .subscribe(result => console.log(result));
+                    //            var observable = Observable.throw(
+                    //            new Error("Something went wrong.")
+                    //        );
+                    //
+                    //        observable.subscribe(
+                    //            x => console.log(x),
+                    //            error => console.error(error)
+                    //        );
+                    var counter = 0;
+                    var gregObservable = Rx_1.Observable.interval(3000);
+                    gregObservable.subscribe(function (x) { return console.log(x); }, console.log("wth"));
+                    var ajaxCall = Rx_1.Observable.of('url')
+                        .flatMap(function () {
+                        if (++counter < 2)
+                            return Rx_1.Observable.throw(new Error("Request failed!"));
                         return Rx_1.Observable.of([1, 2, 3]);
-                    })
-                        .subscribe(function (x) { return console.log(x); });
-                    //        observable.subscribe(x => console.log(x));
-                    //        console.log("observable = ", observable);
+                    });
+                    ajaxCall.retry().subscribe(function (x) { return console.log(x); }, function (error) { return console.error(error); });
                 }
                 AppComponent = __decorate([
                     core_1.Component({
--- app/app.component.ts
+++ app/app.component.ts
@@ -49,12 +49,56 @@ export class AppComponent {
 
             .subscribe(x => console.log(x));
 
-        var observable = Observable.interval(15000);
-        observable.flatMap(x => {
-            console.log("calling server:");
-            return Observable.of([1, 2, 3]);
-        })
-        .subscribe(x => console.log(x));
+//        var observable = Observable.interval(15000);
+//        observable.flatMap(x => {
+//            console.log("calling server:");
+//            return Observable.of([1, 2, 3]);
+//        })
+//        .subscribe(x => console.log(x));
+
+//        var userStream = Observable.of({
+//            userId:     1,
+//            username:   'mosh'
+//        }).delay(2000);
+//
+//        var tweetsStream = Observable.of([1, 2, 3]).delay(1500);
+//
+//        Observable
+//            .forkJoin(userStream, tweetsStream)
+//            .map(joined => 
+//                 new Object({user: joined[0], tweets: joined[1]})
+//                )
+//            .subscribe(result => console.log(result));
+           
+//            var observable = Observable.throw(
+//            new Error("Something went wrong.")
+//        );
+//
+//        observable.subscribe(
+//            x => console.log(x),
+//            error => console.error(error)
+//        );
+
+        var counter = 0;
+
+        var gregObservable = Observable.interval(3000);
+        gregObservable.subscribe(
+            x => console.log(x),
+            console.log("wth"));
+
+
+        var ajaxCall = Observable.of('url')
+            .flatMap(() => {
+                if (++counter < 2)
+                    return Observable.throw(new Error("Request failed!"));
+                return Observable.of([1, 2, 3]);
+            })
+
+        ajaxCall.retry().subscribe(
+            x => console.log(x),
+            error => console.error(error)
+        );
+
 
     }
 }
